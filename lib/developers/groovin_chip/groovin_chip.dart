23f1c65eb633739ff1f0748348d51b3106be79ef
GroovinChip <groovinchip@gmail.com>
2018-12-13 12:11:33 -0500

    updated ios behavior

--- lib/developers/groovin_chip/groovin_chip.dart
+++ lib/developers/groovin_chip/groovin_chip.dart
@@ -97,7 +97,28 @@ class _GroovinChipPageState extends State<GroovinChipPage> {
                                     launch("https://play.google.com/store/apps/details?id=com.groovinchip.flutter.callmanager");
                                   },
                                 )
-                              : Container(),
+                              : Builder(
+                                builder: (BuildContext context) {
+                                  return IconButton(
+                                    icon: Icon(GroovinMaterialIcons.apple),
+                                    onPressed: () {
+                                      Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Row(
+                                          children: <Widget>[
+                                            Icon(Icons.info_outline),
+                                            Padding(
+                                              padding: const EdgeInsets.only(left: 10.0),
+                                              child: Text("Not avaialble on iOS yet"),
+                                            ),
+                                          ],
+                                        ),
+                                        duration: Duration(seconds: 2),
+                                      )
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
                               IconButton(
                                 icon: Icon(
                                   GroovinMaterialIcons.xda,
